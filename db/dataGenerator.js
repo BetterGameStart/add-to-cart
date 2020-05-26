f9f3f9301d4d0b259b65e0492af7766d72905aa5
Jeff Maughan <geophray@gmail.com>
2020-05-26 14:35:57 -0700

    Create Game model in postgres index.js

--- db/dataGenerator.js
+++ db/dataGenerator.js
@@ -52,25 +52,3 @@ writeTenMillionRecords(writeData, 'utf-8', () => {
   const elapsed = new Date().getTime() - start;
   console.log('All finished! Duration (ms): ', elapsed);
 });
-
-
-
-
-// // Create the table schema
-// CREATE TABLE games (
-//   id SERIAL PRIMARY KEY,
-//   gameId int UNIQUE NOT NULL,
-//   title varchar(80),
-//   publisher varchar(80),
-//   reviewScore smallint CHECK (reviewScore >= 0 AND reviewScore < 6),
-//   reviewCount int CHECK (reviewCount >= 0),
-//   ageRating smallint CHECK (ageRating > 0 AND ageRating < 7),
-//   newPrice numeric CHECK (newPrice > 0),
-//   usedPrice numeric CHECK (newPrice > usedPrice AND usedPrice > 0),
-//   digitalPrice numeric CHECK (digitalPrice > 0),
-//   storeLocation varchar(160),
-//   inStock boolean
-// );
-// // To copy the contents of the csv into the games table
-// COPY games(gameId, title, publisher, reviewScore, reviewCount, ageRating, newPrice, usedPrice, digitalPrice, storeLocation, inStock)
-// FROM '/home/geophreigh/Documents/Hack-Reactor/sdc/add-to-cart/db/postgres/data.csv' DELIMITER ',' CSV HEADER;

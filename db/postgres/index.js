cf39902bd11a299e07e42438bc76a3d136807fa4
Jeff Maughan <geophray@gmail.com>
2020-05-28 14:12:01 -0700

    Update existing records via REST api

--- db/postgres/index.js
+++ db/postgres/index.js
@@ -35,6 +35,17 @@ const getGame = (gameId, cb) => {
 
 const updateGame = (gameId, game, cb) => {
   // Update an existing record in the games table by gameId
+  pool.connect((err, client, done) => {
+    const { gameId, title, publisher, reviewScore, reviewCount, ageRating, newPrice, usedPrice, digitalPrice, storeLocation, inStock } = game;
+    const text = 'UPDATE games SET "title" = $2, "publisher" = $3, "reviewScore" = $4, "reviewCount" = $5, "ageRating" = $6, "newPrice" = $7, "usedPrice" = $8, "digitalPrice" = $9, "storeLocation" = $10, "inStock" = $11 WHERE "gameId" = $1';
+    const values = [gameId, title, publisher, reviewScore, reviewCount, ageRating, newPrice, usedPrice, digitalPrice, storeLocation, inStock];
+    if (err) throw err;
+    client.query(text, values, (err, res) => {
+      console.log('Error: ', err, '\nResults: ', res);
+      done();
+      cb(err, res);
+    })
+  })
 };
 
 const deleteGame = (gameId, cb) => {
@@ -63,12 +74,10 @@ const deleteGame = (gameId, cb) => {
 //   })
 // };
 
-deleteAll();
-
 module.exports = {
   addGame,
   getGame,
   updateGame,
   deleteGame,
-  deleteAll,
+  // deleteAll,
 };

527367ba2cc27df365de23581f7d8da5a35661da
Jeff Maughan <geophray@gmail.com>
2020-05-27 20:34:53 -0700

    Use quoted column names throughout app to preserve camelCasing

--- db/postgres/config.js
+++ db/postgres/config.js
@@ -9,23 +9,28 @@ const pool = new Pool({
   port: process.env.DB_PORT,
 });
 
+pool.on('error', (err, client) => {
+  console.error('Unexpected error on idle client', err)
+  process.exit(-1)
+})
+
 module.exports = pool;
 
 // // Create the table schema
 // CREATE TABLE games (
-//   id SERIAL PRIMARY KEY,
-//   gameId int UNIQUE NOT NULL,
-//   title varchar(80),
-//   publisher varchar(80),
-//   reviewScore smallint CHECK (reviewScore >= 0 AND reviewScore < 6),
-//   reviewCount int CHECK (reviewCount >= 0),
-//   ageRating smallint CHECK (ageRating > 0 AND ageRating < 7),
-//   newPrice numeric CHECK (newPrice > 0),
-//   usedPrice numeric CHECK (newPrice > usedPrice AND usedPrice > 0),
-//   digitalPrice numeric CHECK (digitalPrice > 0),
-//   storeLocation varchar(160),
-//   inStock boolean
+//   "id" SERIAL PRIMARY KEY,
+//   "gameId" int UNIQUE NOT NULL,
+//   "title" varchar(80),
+//   "publisher" varchar(80),
+//   "reviewScore" smallint CHECK ("reviewScore" >= 0 AND "reviewScore" < 6),
+//   "reviewCount" int CHECK ("reviewCount" >= 0),
+//   "ageRating" smallint CHECK ("ageRating" > 0 AND "ageRating" < 7),
+//   "newPrice" numeric CHECK ("newPrice" > 0),
+//   "usedPrice" numeric CHECK ("newPrice" > "usedPrice" AND "usedPrice" > 0),
+//   "digitalPrice" numeric CHECK ("digitalPrice" > 0),
+//   "storeLocation" varchar(160),
+//   "inStock" boolean
 // );
 // // To copy the contents of the csv into the games table
-// COPY games(gameId, title, publisher, reviewScore, reviewCount, ageRating, newPrice, usedPrice, digitalPrice, storeLocation, inStock)
+// COPY games("gameId", "title", "publisher", "reviewScore", "reviewCount", "ageRating", "newPrice", "usedPrice", "digitalPrice", "storeLocation", "inStock")
 // FROM '/home/geophreigh/Documents/Hack-Reactor/sdc/add-to-cart/db/data.csv' DELIMITER '|' CSV HEADER;

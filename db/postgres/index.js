aeae9d70430a6cf4761091a7596005759204f8e0
Jeff Maughan <geophray@gmail.com>
2020-05-27 21:01:04 -0700

    Add new record to games table via REST api

--- db/postgres/index.js
+++ db/postgres/index.js
@@ -7,12 +7,22 @@ const pool = require('./config.js');
 
 const addGame = (game, cb) => {
   // Add a new record to the games table
+  pool.connect((err, client, done) => {
+    const { gameId, title, publisher, reviewScore, reviewCount, ageRating, newPrice, usedPrice, digitalPrice, storeLocation, inStock } = game;
+    const text = 'INSERT INTO games ("gameId", "title", "publisher", "reviewScore", "reviewCount", "ageRating", "newPrice", "usedPrice", "digitalPrice", "storeLocation", "inStock") VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)';
+    const values = [gameId, title, publisher, reviewScore, reviewCount, ageRating, newPrice, usedPrice, digitalPrice, storeLocation, inStock];
+    if (err) throw err;
+    client.query(text, values, (err, res) => {
+      console.log('Error: ', err, '\nResults: ', res);
+      done();
+      cb(err, res);
+    })
+  })
 };
 
 const getGame = (gameId, cb) => {
   // Get a single record from the games table by gameId
   pool.connect((err, client, done) => {
-    console.log('gameId from inside getGame: ', gameId);
     const text = 'SELECT * FROM games WHERE "gameId" = $1';
     const values = [gameId];
     if (err) throw err;

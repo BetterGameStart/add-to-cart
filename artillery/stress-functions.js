8982fffff1e75097a6fdf584ad1c7bbf4cf5802e
Jeff Maughan <geophray@gmail.com>
2020-05-30 17:59:29 -0700

    Add artillery tests for get/post endpoints

--- artillery/stress-functions.js
+++ artillery/stress-functions.js
@@ -1,6 +1,6 @@
 const faker = require('faker');
 
-var id = 10000003;
+var id = 10336801;
 
 const generateHighId = (context, events, done) => {
   const reqId = Math.floor(Math.random() * (id - (id * 0.75))) + Math.floor(id * 0.75);
@@ -15,21 +15,20 @@ const generateLowId = (context, events, done) => {
 };
 
 const generateNewGame = (context, events, done) => {
-  const game = {
-    "gameId": id,
-    "title": faker.fake('{{company.catchPhraseAdjective}} {{company.bsNoun}}'),
-    "publisher": faker.fake('{{company.companyName}}'),
-    "reviewScore": Math.ceil(Math.random() * 5),
-    "reviewCount": Math.ceil(Math.random() * 5000),
-    "ageRating": Math.ceil(Math.random() * 6),
-    "newPrice": 60,
-    "usedPrice": Math.ceil(Math.random() * 55),
-    "digitalPrice": Math.floor(Math.random() * (60 - 45 + 1) + 45),
-    "storeLocation": faker.fake('{{address.streetAddress}} {{address.streetName}} {{address.city}}, {{address.state}}'),
-    "inStock": faker.random.boolean(),
-  }
+  context.vars.gameId = id;
+  context.vars.title = faker.fake('{{company.catchPhraseAdjective}} {{company.bsNoun}}');
+  context.vars.publisher = faker.fake('{{company.companyName}}');
+  context.vars.reviewScore = Math.ceil(Math.random() * 5);
+  context.vars.reviewCount = Math.ceil(Math.random() * 5000);
+  context.vars.ageRating = Math.ceil(Math.random() * 6);
+  context.vars.newPrice = 60;
+  context.vars.usedPrice = Math.ceil(Math.random() * 55);
+  context.vars.digitalPrice = Math.floor(Math.random() * (60 - 45 + 1) + 45);
+  context.vars.storeLocation = faker.fake('{{address.streetAddress}} {{address.streetName}} {{address.city}}, {{address.state}}');
+  context.vars.inStock = faker.random.boolean();
+
   id++;
-  context.vars.reqBod = JSON.stringify(game);
+
   return done();
 };
 
